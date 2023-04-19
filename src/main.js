ced3657e441b95724bac4625df71478f331b3f00
Giuseppe Indomenico <pippo.son@hotmail.com>
2023-04-19 12:22:14 +0200

    add milestone

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import 'bootstrap/dist/css/bootstrap.css'
 import './assets/general.scss'
 
 createApp(App).mount('#app')
+
+
+
+//Key API: 2010639fc959155bb3be029b450b5c82
+
+//baseURL: https://api.themoviedb.org/3
