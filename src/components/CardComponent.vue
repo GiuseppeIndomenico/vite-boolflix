1aa38cc097fa581174a9261cfbf4f2828aa21e2a
Giuseppe Indomenico <pippo.son@hotmail.com>
2023-04-20 10:20:41 +0200

    fix data#3

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -18,20 +18,8 @@ export default {
         GetPopular() {
             axios.get(store.Url.base + store.endPoint.popular + store.key).then(res => {
                 //console.log(res.data.results);
-                const infos = res.data.results;
-
-                for (let i = 0; i < infos.length; i++) {
-                    const element = infos[i];
-                    const infoMovies = {
-                        id: element.id,
-                        title: element.title,
-                        name:element.name,
-                        largeImg: element.backdrop_path,
-                        posterImg: element.poster_path,
-                        overview: element.overview,
-                    };
-                    store.movieTrend.push(infoMovies);
-                };
+                store.movieTrend = res.data.results;
+                   
                 console.log(store.movieTrend);
             })
         },
--- src/data/Store.js
+++ src/data/Store.js
@@ -6,10 +6,17 @@ export const store = reactive({
         imgThumb: 'https://image.tmdb.org/t/p/w500/',
     },
     endPoint: {
-        popular: '/trending/all/day?'
+        popular: '/trending/all/day?',
+        movie:'/search/movie'
     },
     key: 'api_key=2010639fc959155bb3be029b450b5c82',
     movieTrend: [],
+
+    params:{
+        api_key:'',//inserire la mia key
+       // query:'a'
+
+    }
 })
 
 //Key API: 2010639fc959155bb3be029b450b5c82
