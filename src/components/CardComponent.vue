22fe18825c57cad213dc8607a512eb64565a66fc
Giuseppe Indomenico <pippo.son@hotmail.com>
2023-04-20 09:31:08 +0200

    first take array data

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -11,26 +11,38 @@ export default {
     data() {
         return {
             store,
-          
+
         }
     },
     methods: {
         GetPopular() {
-            axios.get(store.Url.base + store.endPoint.popular + store.key).then(res=>{
-                console.log(res.data.results);
-                
-                //sono un cretino...mancava uno slash
-                    
+            axios.get(store.Url.base + store.endPoint.popular + store.key).then(res => {
+                //console.log(res.data.results);
+                const infos = res.data.results;
 
-
+                for (let i = 0; i < infos.length; i++) {
+                    const element = infos[i];
+                    const infoMovies = {
+                        id: element.id,
+                        title: element.title,
+                        largeImg: element.backdrop_path,
+                        posterImg: element.poster_path,
+                        overwiew: element.overwiew,
+                    };
+                    store.movieTrend.push(infoMovies);
+                };
+                console.log(store.movieTrend);
             })
-        }
-    },mounted(){
+        },
+
+
+    },
+    mounted() {
         this.GetPopular()
     }
-
+    
+    
 }
-
 </script>
 
 <style lang="scss" scoped></style>
